b2b3a18af01c1e080ffb173234a3cf02bbe03465
shoxizuz <infotrustedrepair@gmail.com>
2024-11-07 22:07:45 -0500

    my own commit

---
+++ src/main/java/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("My new git project");
+        System.out.println("My very own project");
+    }
+}
